2c6c914354fbb4ba410b9ee30ce738f093b70621
Ernestso1998 <135281894+Ernestso1998@users.noreply.github.com>
2023-07-30 01:16:07 +0000

    Question #1

--- src/die.cpp
+++ src/die.cpp
@@ -1,1 +1,19 @@
-//
+#include <iostream>
+#include <cstdlib>
+#include <random>
+#include "die.h"
+
+
+void Die::roll()
+{
+    std::random_device rd;
+    std::uniform_int_distribution<int>sides(1, 6);
+        
+    roll_value = sides(rd); 
+
+}
+
+int Die::rolled_value()const 
+{ 
+    return roll_value; 
+}
--- src/die.h
+++ src/die.h
@@ -1,1 +1,11 @@
-//
+#include <iostream>
+#include <cstdlib>
+class Die
+{
+public:
+    void roll();
+    int rolled_value() const;
+private:
+    int roll_value;
+    int sides{6};
+};
