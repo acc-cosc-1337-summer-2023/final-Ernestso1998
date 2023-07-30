565a5c301922864188dbcabe2b5ea60fe89383f7
Ernestso1998 <135281894+Ernestso1998@users.noreply.github.com>
2023-07-30 03:31:55 +0000

    Question #3

--- src/shooter.h
+++ src/shooter.h
@@ -1,1 +1,14 @@
-//
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include "roll.h"
+class Shooter
+{
+public:
+	Roll* throw_die(Die& die1, Die& die2);
+	~Shooter();
+	friend std::ostream& operator<<(std::ostream& os, const Shooter& shooter);
+private:
+	std::vector<Roll*> rolls;
+};
+
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,7 +1,11 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include <iostream>
 #include "roll.h"
 #include "die.h"
+#include "shooter.h"
+#include "come_out_phase.h"
+#include "point_phase.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -33,4 +37,78 @@ Roll roll(die1, die2);
     }
 }
 
+TEST_CASE("Testing Shooter return a Roll") {
+	Shooter shoot;
+	Die die1, die2;
 
+	for(int i = 0; i < 10; i++){
+
+		Roll* roll = shoot.throw_die(die1, die2);
+		REQUIRE(roll->roll_value() >= 2);
+		REQUIRE(roll->roll_value() <= 12);
+	}
+}
+
+TEST_CASE("ComeOutPhase Test")
+{
+	Phase_Comeout out;
+	Die die1, die2;
+
+
+	Roll* dieroll = new Roll(die1, die2);
+
+	for (int i = 0; i < 10; ++i) 
+	{
+		dieroll->roll_die();
+
+		if(dieroll->roll_value() == 7 )
+		{
+			REQUIRE(out.get_outcome(dieroll)== RollOutcome::natural);
+		}
+		else if(dieroll->roll_value() == 11)
+		{
+			REQUIRE(out.get_outcome(dieroll)== RollOutcome::natural);
+		}
+		else if(dieroll->roll_value() == 2)
+		{
+			REQUIRE(out.get_outcome(dieroll)== RollOutcome::craps);
+		}
+		else if(dieroll->roll_value() == 3)
+		{
+			REQUIRE(out.get_outcome(dieroll)== RollOutcome::craps);
+		}
+		else if(dieroll->roll_value() == 12)
+		{
+			REQUIRE(out.get_outcome(dieroll)== RollOutcome::craps);
+		}
+		else
+			REQUIRE(out.get_outcome(dieroll)== RollOutcome::point);
+	}
+}
+
+
+TEST_CASE("POINT PHASE")
+{
+	PointPhase p(4);
+	Die die1, die2;
+
+
+	Roll* diceroll = new Roll(die1, die2);
+
+	for (int i = 0; i < 10; ++i) 
+	{
+		diceroll->roll_die();
+
+		if(diceroll->roll_value() == 4)
+		{
+			REQUIRE(p.get_outcome(diceroll) == RollOutcome::point);
+		}
+		else if(diceroll->roll_value() == 7){
+			REQUIRE(p.get_outcome(diceroll) == RollOutcome::seven_out);
+		}
+		else
+		{
+			REQUIRE(p.get_outcome(diceroll) == RollOutcome::nopoint);
+		}
+	}
+}
