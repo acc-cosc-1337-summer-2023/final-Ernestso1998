de4ab15925ecdf9ce2ab947292ac40953be2a968
Ernestso1998 <135281894+Ernestso1998@users.noreply.github.com>
2023-07-30 01:16:41 +0000

    Question #1 Test

--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,6 +1,30 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "die.h"
+#include "roll.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
+
+TEST_CASE("Die Value Test")
+{
+	Die die;
+
+	for(int i=0; i<10; i++)
+	{
+		REQUIRE(die.rolled_value()> 0);
+		REQUIRE(die.rolled_value()<= 6);
+	}
+	
+}
+
+TEST_CASE("Roll Value TEST")
+{
+	Roll sum;
+
+	
+
+
+}
+
